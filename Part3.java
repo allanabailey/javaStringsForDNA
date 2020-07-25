3d232d6c753b8e9768b619c91da71500a84e84d1
Allana Bailey <47748433+allanabailey@users.noreply.github.com>
2020-07-25 20:53:08 +0100

    Add files via upload

---
+++ Part3.java
@@ -0,0 +1,52 @@
+package StringsClass1;
+
+
+/**
+ * Write a description of Part3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part3 {
+    public boolean twoOccurrences(String stringa, String stringb) {
+        int count = 0;
+        int fromIndex = 0;
+        while((fromIndex = stringb.indexOf(stringa, fromIndex)) != -1) {
+            count ++;
+            fromIndex++;
+        }
+        if(count >= 2) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void testing() {
+        String stringa1 = "by";
+        String stringb1 = "A story by Abby Long";
+        String stringa2 = "a";
+        String stringb2 = "banana";
+        String stringa3 = "atg";
+        String stringb3 = "ctgtatgta";
+        twoOccurrences(stringa1, stringb1);
+        System.out.println("Does " + stringa1 + " appear in " + stringb1 + " at least twice?");
+        System.out.println(twoOccurrences(stringa1, stringb1));
+        twoOccurrences(stringa2, stringb2);
+        System.out.println("Does " + stringa2 + " appear in " + stringb2 + " at least twice?");
+        System.out.println(twoOccurrences(stringa2, stringb2));
+        twoOccurrences(stringa3, stringb3);
+        System.out.println("Does " + stringa3 + " appear in " + stringb3 + " at least twice?");
+        System.out.println(twoOccurrences(stringa3, stringb3));
+    }
+    
+    public String lastPart(String stringa, String stringb) {
+        int index = stringb.indexOf(stringa);
+        if(index == -1) {
+            System.out.println(stringa + " does not appear in " + stringb);
+            return stringb;
+        } else {
+            System.out.println("The part of the string after " + stringa + " in " + stringb + " is " + stringb.substring(index + stringa.length())); 
+            return stringb.substring(index + stringa.length());
+        }
+    }
+}
