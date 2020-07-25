cff1e638668f240eb99f5336a97209b35ad3c4a5
Allana Bailey <47748433+allanabailey@users.noreply.github.com>
2020-07-25 20:47:20 +0100

    Add files via upload

---
+++ Part1.java
@@ -0,0 +1,51 @@
+package StringsClass1;
+
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part1 {
+    public String findSimpleGene(String dna) {
+        int startIndex = dna.indexOf("ATG");
+        if(startIndex == -1) {
+            return "";
+        }
+        int stopIndex = dna.indexOf("TAA", startIndex+3);
+        if(stopIndex == -1) {
+            return "";
+        }
+        String result = dna.substring(startIndex, stopIndex+3);
+        if(result.length() % 3 == 0) {
+            return result;
+        } else {
+            return "";
+        }
+    }
+    
+    public void testSimpleGene() {
+        //ATG and TAA present and multiple of 3
+        String test1 = "ACGTATGCGCTAAGC";
+        System.out.println("DNA String: " + test1);
+        String result1 = findSimpleGene(test1);
+        System.out.println("Gene: " + result1);
+        //ATG and TAA present but not multiple of 3
+        String test2 = "ACGTATGCGTAAGC";
+        System.out.println("DNA String: " + test2);
+        String result2 = findSimpleGene(test2);
+        System.out.println("Gene: " + result2);
+        //No ATG present
+        String test3 = "AGCTCCGGTAAGCT";
+        System.out.println("DNA String: " + test3);
+        String result3 = findSimpleGene(test3);
+        System.out.println("Gene: " + result3);
+        //No TAA present
+        String test4 = "AGCTTGCCAAATGC";
+        System.out.println("DNA String: " + test4);
+        String result4 = findSimpleGene(test4);
+        System.out.println("Gene: " + result4);
+        
+    }
+}
